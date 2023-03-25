6fe439531ad0777d38eeec89066ad1443449366c
mannyrveloz23 <you@example.com>
2023-03-24 22:35:31 -0400

    update my script to use Jquery

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,54 +1,55 @@
-const searchBtn = document.querySelector('.fa');
-const searchInput = document.querySelector('#input');
-const searchHistory = document.querySelector('.search-history');
+const searchBtn = $('.fa');
+const searchInput = $('#input');
+const searchHistory = $('.search-history');
 
-// let requestUrl = "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={22e14a0417438591076e071dfd21f126}"
+$.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=Orlando,Florida&appid=22e14a0417438591076e071dfd21f126',
+    dataType: 'json',
+    success: function(data) {
+        console.log(data);
+    },
+    error: function(error) {
+        console.log(error)
+    }
+});
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Orlando,Florida&appid=22e14a0417438591076e071dfd21f126')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
 
-
-searchBtn.addEventListener('click', () => {
+searchBtn.on('click', function() {
     recentSearch();
 });
 
 // add an event listener to the search input field
-searchInput.addEventListener('keydown', (event) => {
+searchInput.on('keydown', function (event) {
     // check if the key pressed is the Enter key
     if (event.keyCode === 13) {
         // trigger the click event on the search button
-        searchBtn.click();
+        searchBtn.trigger('click');
     }
 });
 
-const recentSearch = () => {
-    const searchTerm = searchInput.value;
+const recentSearch = function() {
+    const searchTerm = searchInput.val;
 
     // create a new li element with the search term as its text content
-    const newSearchItem = document.createElement('p');
-    newSearchItem.classList.add("light-text");
-    newSearchItem.classList.add("suggestion");
-    newSearchItem.textContent = searchTerm;
+    const newSearchItem = $('<p class="light-text suggestion">' + searchTerm + '');
 
       // add an event listener to the new li element
-      newSearchItem.addEventListener('click', () => {
+      newSearchItem.on('click', function() {
         // retrieve the search term from the text content of the clicked li element
-        const clickedTerm = newSearchItem.textContent;
+        const clickedTerm = $(this).text();
 
         // set the search input field value to the clicked term
-        searchInput.value = clickedTerm;
+        searchInput.val(clickedTerm);
 
         // perform the search again
         // your code here
     });
 
     // append the new li element to the search history container
-    searchHistory.appendChild(newSearchItem);
+    searchHistory.append(newSearchItem);
 
     // clear the search input field
-    searchInput.value = '';
+    searchInput.val('');
 
 };
 
