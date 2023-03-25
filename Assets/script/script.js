c1b4f919edb7dfffc4bb8ab5ea144ba1efe97e25
mannyrveloz23 <you@example.com>
2023-03-24 21:58:31 -0400

    fetch api added

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,6 +1,18 @@
-const searchBtn = document.querySelector('.btn-primary');
-const searchInput = document.querySelector('.form-control');
-const searchHistory = document.querySelector('#search-history');
+const searchBtn = document.querySelector('.fa');
+const searchInput = document.querySelector('#input');
+const searchHistory = document.querySelector('.search-history');
+
+// let requestUrl = "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={22e14a0417438591076e071dfd21f126}"
+
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Orlando,Florida&appid=22e14a0417438591076e071dfd21f126')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+
+searchBtn.addEventListener('click', () => {
+    recentSearch();
+});
 
 // add an event listener to the search input field
 searchInput.addEventListener('keydown', (event) => {
@@ -11,11 +23,13 @@ searchInput.addEventListener('keydown', (event) => {
     }
 });
 
-searchBtn.addEventListener('click', () => {
+const recentSearch = () => {
     const searchTerm = searchInput.value;
 
     // create a new li element with the search term as its text content
-    const newSearchItem = document.createElement('li');
+    const newSearchItem = document.createElement('p');
+    newSearchItem.classList.add("light-text");
+    newSearchItem.classList.add("suggestion");
     newSearchItem.textContent = searchTerm;
 
       // add an event listener to the new li element
@@ -35,5 +49,8 @@ searchBtn.addEventListener('click', () => {
 
     // clear the search input field
     searchInput.value = '';
-});
+
+};
+
+
 
